55213f9c4a464e4cac07fd93f5dd19b9ed01348b
Brian McCandless <bmcandles@gmail.com>
2021-04-03 13:37:24 -0400

    initialize project

---
+++ apollo-missions-rest/src/main/java/com/bemccandless/apollomissions/ApolloMissionsApplication.java
@@ -0,0 +1,13 @@
+package com.bemccandless.apollomissions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApolloMissionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApolloMissionsApplication.class, args);
+	}
+
+}
---
+++ apollo-missions-rest/src/test/java/com/bemccandless/apollomissions/ApolloMissionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bemccandless.apollomissions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApolloMissionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
